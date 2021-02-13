fdda58a53a4a54834f51cd291028ae157fca4ea3
lmartin <martin.ly@northeastern.edu>
2021-02-12 22:37:10 -0500

    try to use mocks

--- Snarl/Game/src/test/java/model/HallwayBuilderTest.java
+++ Snarl/Game/src/test/java/model/HallwayBuilderTest.java
@@ -2,12 +2,25 @@ package model;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class HallwayBuilderTest {
+
+  @Mock(name="to")
+  private RoomBuilder to;
+  @Mock(name="from")
+  private RoomBuilder from;
+  @Spy
+  private ArrayList<Location> waypoints = new ArrayList<>();
+  @InjectMocks
+  private HallwayBuilder hallwayMocked;
 
   private ArrayList<ArrayList<Space>> spaces;
   private RoomBuilder roomX1Y1;
@@ -18,6 +31,7 @@ public class HallwayBuilderTest {
     this.spaces = new ArrayList<>();
     this.roomX1Y1 = new RoomBuilder(1, 1, 1, 1).door(2, 1);
     this.roomX5Y5 = new RoomBuilder(5, 5, 1, 1).door(5, 4);
+    this.hallwayMocked = new HallwayBuilder(this.roomX1Y1, this.roomX5Y5);
   }
 
   @Test
@@ -69,5 +83,12 @@ public class HallwayBuilderTest {
     catch (IllegalArgumentException e) {
       assertEquals("locations must be on one of the same axes", e.getMessage());
     }
+  }
+
+  @Test
+  public void testWaypoints() {
+    // currently failing because the injection isn't working - need to figure out these mocks
+    hallwayMocked.waypoint(5, 1);
+    Mockito.verify(waypoints).add(new Location(3, 1));
   }
 }
