96b7bec1f94cabc4d50bbfc52839e12b53db8f65
lmartin <martin.ly@northeastern.edu>
2021-03-09 21:01:14 -0600

    fix merge mistake

--- Snarl/Game/src/main/java/view/ASCIIView.java
+++ Snarl/Game/src/main/java/view/ASCIIView.java
@@ -41,20 +41,6 @@ public class ASCIIView implements View {
   }
 
   /**
-<<<<<<< HEAD
-=======
-   * Places the items in the 2D game representation.
-   * @param items the items to place
-   */
-  public void placeItems(List<Item> items) {
-    items.forEach(item -> {
-      Location location = item.getCurrentLocation();
-      this.render.get(location.row).set(location.column, item.acceptVisitor(new ASCIIItem()));
-    });
-  }
-
-  /**
->>>>>>> ddd35ccb021eb93c40afbe3ec7057941d2647a28
    * Outputs the drawn view to System.out.
    */
   @Override
